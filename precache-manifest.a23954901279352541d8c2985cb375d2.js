8abcb74ce409ffdd825a38bdd485b7d7dd3300cc
Harsith Priyan Sivanandham <harsithpriyan@gmail.com>
2020-06-19 16:47:10 +0530

    Updates

---
+++ precache-manifest.a23954901279352541d8c2985cb375d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e87f9e459f9f1dd5511c3213535e0a8b",
+    "url": "/React-to-do-list-app/index.html"
+  },
+  {
+    "revision": "0c9a456389eaeaf374f2",
+    "url": "/React-to-do-list-app/static/css/main.11a053cc.chunk.css"
+  },
+  {
+    "revision": "08fbc5f87fd1e609c90d",
+    "url": "/React-to-do-list-app/static/js/2.ae5df145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-to-do-list-app/static/js/2.ae5df145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c9a456389eaeaf374f2",
+    "url": "/React-to-do-list-app/static/js/main.d378b1fe.chunk.js"
+  },
+  {
+    "revision": "7d2eb028c9479b94f50a",
+    "url": "/React-to-do-list-app/static/js/runtime-main.cbab8d16.js"
+  }
+]);
